99de47657a6b25fe767d6c43119bda831cfbb81c
Sailesh Kasaju <leosailesh@gmail.com>
2026-09-14 10:18:22 +0545

    feat: upgrade to Tailwind CSS v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	ssr: {
 		noExternal: ['three']
 	},
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 		Icons({
 			compiler: 'svelte',
